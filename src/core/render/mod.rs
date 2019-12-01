ce85589825863eee7e2401b2e994089797198ddd
qwertz19281 <dellen903@gmail.com>
2019-12-01 23:00:53 +0100

    attempt to fix render fns

--- src/core/render/mod.rs
+++ src/core/render/mod.rs
@@ -7,27 +7,6 @@ use crate::core::env::WidgetStore;
 use std::any::TypeId;
 
 pub trait Render {
-    #[inline]
-    fn requires_render<W: Widget<E>, E: Env>(&self, w: &W) -> bool {
-        w.render() || self.force()
-    }
-
-    fn render_widgets<'a,E: Env,W: IBoundedWidget<E> + 'a>(&mut self, i: impl Iterator<Item=&'a W>, c: &mut E::Ctx, overlap: bool) {
-        let senf: &mut E::Renderer = hackcast_mut(self).expect("Differenting Renderer");
-        
-        if overlap {
-            let mut render = false;
-            for w in i {
-                let ww = c.widgets_mut().get_mut(&w.id()).expect("Lost Child");
-                render |= senf.requires_render(&ww);
-                if render {
-                    ww.handler().render(c,senf.slice(w.bounds()));
-                }
-                render &= overlap;
-            }
-        }
-    }
-
     fn bounds_abs(&self) -> Bounds;
     fn slice(&mut self, b: &Bounds) -> Self;
 
@@ -44,12 +23,29 @@ pub trait Render {
     fn border_rect_rgba(&mut self, c: [u8;4], thickness: u32);
 }
 
-//fn a(e: Box<dyn Render<Sliced=(dyn Render)>>) {}
+pub trait RenderExt<E> where E: Env {
+    fn requires_render(&self, w: &E::DynWidget) -> bool;
 
-fn hackcast_mut<'a,T,U>(t: &'a mut T) -> Option<&'a mut U> {
-    if TypeId::of::<T>() == TypeId::of::<U>() {
-        Some( unsafe { *(t as *mut T as *mut U) } )
-    }else{
-        None
+    fn render_widgets<'a,W: IBoundedWidget<E> + 'a>(&mut self, i: impl Iterator<Item=&'a W>, c: &mut E::Ctx, overlap: bool);
+}
+
+impl<E> RenderExt<E> for E::Renderer where E: Env {
+    #[inline]
+    fn requires_render(&self, w: &E::DynWidget) -> bool {
+        w.render() || self.force()
+    }
+
+    fn render_widgets<'a,W: IBoundedWidget<E> + 'a>(&mut self, i: impl Iterator<Item=&'a W>, c: &mut E::Ctx, overlap: bool) {
+        if overlap {
+            let mut render = false;
+            for w in i {
+                let ww = c.widgets_mut().get_mut(&w.id()).expect("Lost Child");
+                render |= self.requires_render(&ww);
+                if render {
+                    ww.handler().render(c,self.slice(w.bounds()));
+                }
+                render &= overlap;
+            }
+        }
     }
 }
