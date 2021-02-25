739b6d1aa8a5d78e14794f957c5ecb3dc118996e
qwertz19281 <dellen903@gmail.com>
2021-02-25 16:02:39 +0100

    Widget::tabulate implementation

--- src/handler/standard/imp.rs
+++ src/handler/standard/imp.rs
@@ -104,7 +104,8 @@ impl<S,E> Handler<E> for StdHandler<S,E> where
                             if let Ok(mut l) = l.with_widget(id.path.refc()) {
                                 if key == EEKey::<E>::TAB && l.widget._tabulate_by_tab() {
                                     let reverse = l.state().is_pressed(&[EEKey::<E>::SHIFT]).is_some();
-                                    let path = tabulate::<E>(l.widget.stor,id.path,reverse);
+                                    let dir = if reverse {TabulateDirection::Backward} else {TabulateDirection::Forward};
+                                    let path = tabi(l.reference(),id.path,dir).expect("TODO");
                                     //better way than this hack to get the ident
                                     l.as_mut().s.kbd.focused = Some(WidgetIdent::from_path(path,l.widget.stor).expect("TODO"));
                                 }
--- src/util/tabulate.rs
+++ src/util/tabulate.rs
@@ -1,10 +1,33 @@
+use super::*;
+
+#[derive(Clone,Copy)]
 pub enum TabulateDirection { //TODO trait
-    Forward(),
-    Backward(),
+    Forward,
+    Backward,
 }
 
 pub enum TabulateOrigin<E> where E: Env {
-    Parent(),
-    Child(E::WidgetPath),
+    Resolve(E::WidgetPath),
+    Enter,
 }
 
+pub enum TabulateResponse<E> where E: Env {
+    Done(E::WidgetPath),
+    Leave,
+}
+
+pub fn tabi<E>(mut root: Link<E>, sub_path: E::WidgetPath, dir: TabulateDirection) -> Result<E::WidgetPath,GuionError<E>> where E: Env {
+    let mut current = sub_path.clone(); //TODO sub_path to absolute path WidgetPath::strip_prefix
+    let result = root._tabulate(TabulateOrigin::Resolve(sub_path),dir)?;
+    match result {
+        TabulateResponse::Done(p) => current = p,
+        TabulateResponse::Leave => {
+            let result = root._tabulate(TabulateOrigin::Enter,dir)?;
+            match result {
+                TabulateResponse::Done(p) => current = p,
+                TabulateResponse::Leave => {},
+            }
+        },
+    }
+    Ok(current)
+}
