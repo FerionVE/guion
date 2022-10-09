d63d091d28665580a0281b9ddface99b60fc6f1b
qwertz19281 <dellen903@gmail.com>
2022-10-10 00:00:08 +0200

    Partially fix ViewWidget

--- src/view/view_widget.rs
+++ src/view/view_widget.rs
@@ -13,12 +13,12 @@ use super::{View, box_view_cb};
 use super::mut_target::MuTarget;
 use super::mutor_trait::*;
 
-pub struct ViewWidget<'a,'b,ViewTy,ViewFn,E>(ViewFn,&'a (dyn MutorToBuilderDyn<(),ViewTy::Mutarget,E>+'b),PhantomData<(ViewTy,E)>) where
+pub struct ViewWidget<'a,ViewTy,ViewFn,E>(ViewFn,Box<dyn MutorToBuilderDyn<(),ViewTy::Mutarget,E>+'a>,PhantomData<(ViewTy,E)>) where
     ViewFn: Fn()->ViewTy,
     ViewTy: View<E>,
     E: Env;
 
-impl<'a,'b,ViewTy,ViewFn,E> AsWidget<E> for ViewWidget<'a,'b,ViewTy,ViewFn,E> where
+impl<'a,ViewTy,ViewFn,E> AsWidget<E> for ViewWidget<'a,ViewTy,ViewFn,E> where
     ViewFn: Fn()->ViewTy,
     ViewTy: View<E>,
     E: Env,
@@ -36,37 +36,37 @@ impl<'a,'b,ViewTy,ViewFn,E> AsWidget<E> for ViewWidget<'a,'b,ViewTy,ViewFn,E> wh
             dispatch.call(widget, root, ctx)
             //todo!()
         });
-        s.view(dis,self.1,root,ctx)
+        s.view(dis,&*self.1,root,ctx)
         //todo!()
     }
 }
 
-// pub fn view_widget_cb<RightView,LeftArgs,LeftMutor,LeftTarget,MutorFn,RightViewFn,E>(v: RightViewFn, a: LeftArgs, m: LeftMutor, f: MutorFn) -> ViewWidget<
-//     RightView,
-//     RightViewFn,
-//     MutorForTarget<RightView::Mutarget,(),MutorForViewCB<LeftMutor,LeftArgs,LeftTarget,RightView::Mutarget,(),MutorFn,E>,E>,
-//     E
-//     >
-// where
-//     E: Env,
-//     RightViewFn: Fn()->RightView,
-//     LeftArgs: Clone + Sized + Send + Sync + 'static,
-//     LeftMutor: MutorTo<LeftArgs,LeftTarget,E>,
-//     LeftTarget: MuTarget<E> + ?Sized,
-//     RightView: View<E>,
-//     MutorFn: for<'s,'ss,'c,'cc> Fn(
-//         ResolveResult<&'s mut LeftTarget::Mutable<'ss>>,&'ss (),
-//         &mut (dyn for<'is,'iss,'ic,'icc> FnMut(ResolveResult<&'is mut <RightView::Mutarget as MuTarget<E>>::Mutable<'iss>>,&'iss (),&'ic mut E::Context<'icc>)),
-//         (),
-//         &'c mut E::Context<'cc>
-//     ) + Clone + Send + Sync + 'static
-// {
-//     ViewWidget(
-//         v,
-//         m.for_view_cb::<RightView::Mutarget,(),MutorFn>(a, f),
-//         PhantomData
-//     )
-// }
+pub fn view_widget_cb<'a,RightView,LeftArgs,LeftMutor,LeftTarget,MutorFn,RightViewFn,E>(v: RightViewFn, a: LeftArgs, m: &'a LeftMutor, f: MutorFn) -> ViewWidget<
+    'a,
+    RightView,
+    RightViewFn,
+    E
+    >
+where
+    E: Env,
+    RightViewFn: Fn()->RightView,
+    LeftArgs: Clone + Sized + Send + Sync + 'static,
+    LeftMutor: MutorToBuilder<LeftArgs,LeftTarget,E>,
+    LeftTarget: MuTarget<E> + ?Sized,
+    RightView: View<E>,
+    MutorFn: for<'s,'ss,'c,'cc> Fn(
+        ResolveResult<&'s mut LeftTarget::Mutable<'ss>>,&'ss (),
+        &mut (dyn for<'is,'iss,'ic,'icc> FnMut(ResolveResult<&'is mut <RightView::Mutarget as MuTarget<E>>::Mutable<'iss>>,&'iss (),&'ic mut E::Context<'icc>)),
+        (),
+        &'c mut E::Context<'cc>
+    ) + Clone + Send + Sync + 'static
+{
+    ViewWidget(
+        v,
+        Box::new(m.for_view_cb::<RightView::Mutarget,(),MutorFn>(a, f)),
+        PhantomData
+    )
+}
 
 // pub fn view_widget_ret<RightView,LeftArgs,LeftMutor,LeftTarget,MutorFn,RightViewFn,E>(v: RightViewFn, a: LeftArgs, m: LeftMutor, f: MutorFn) -> ViewWidget<
 //     RightView,
@@ -121,32 +121,31 @@ impl<'a,'b,ViewTy,ViewFn,E> AsWidget<E> for ViewWidget<'a,'b,ViewTy,ViewFn,E> wh
 //     )
 // }
 
-// pub fn view_widget_cb_if<RightView,LeftArgs,LeftMutor,LeftTarget,MutorFn,RightViewFn,E>(v: RightViewFn, a: LeftArgs, m: LeftMutor, f: MutorFn) -> ViewWidget<
-//     RightView,
-//     RightViewFn,
-//     MutorForTarget<RightView::Mutarget,(),MutorForViewCBIf<LeftMutor,LeftArgs,LeftTarget,RightView::Mutarget,(),MutorFn,E>,E>,
-//     E
-//     >
-// where
-//     E: Env,
-//     RightViewFn: Fn()->RightView,
-//     LeftArgs: Clone + Sized + Send + Sync + 'static,
-//     LeftMutor: MutorTo<LeftArgs,LeftTarget,E>,
-//     LeftTarget: MuTarget<E> + ?Sized,
-//     RightView: View<E>,
-//     MutorFn: for<'s,'ss,'c,'cc> Fn(
-//         &'s mut LeftTarget::Mutable<'ss>,&'ss (),
-//         &mut (dyn for<'is,'iss,'ic,'icc> FnMut(ResolveResult<&'is mut <RightView::Mutarget as MuTarget<E>>::Mutable<'iss>>,&'iss (),&'ic mut E::Context<'icc>)),
-//         (),
-//         &'c mut E::Context<'cc>
-//     ) + Clone + Send + Sync + 'static
-// {
-//     ViewWidget(
-//         v,
-//         m.for_view_cb_if::<RightView::Mutarget,(),MutorFn>(a, f),
-//         PhantomData
-//     )
-// }
+pub fn view_widget_cb_if<'a,RightView,LeftArgs,LeftMutor,LeftTarget,MutorFn,RightViewFn,E>(v: RightViewFn, a: LeftArgs, m: &'a LeftMutor, f: MutorFn) -> ViewWidget<
+    RightView,
+    RightViewFn,
+    E
+    >
+where
+    E: Env,
+    RightViewFn: Fn()->RightView,
+    LeftArgs: Clone + Sized + Send + Sync + 'static,
+    LeftMutor: MutorToBuilder<LeftArgs,LeftTarget,E>,
+    LeftTarget: MuTarget<E> + ?Sized,
+    RightView: View<E>,
+    MutorFn: for<'s,'ss,'c,'cc> Fn(
+        &'s mut LeftTarget::Mutable<'ss>,&'ss (),
+        &mut (dyn for<'is,'iss,'ic,'icc> FnMut(ResolveResult<&'is mut <RightView::Mutarget as MuTarget<E>>::Mutable<'iss>>,&'iss (),&'ic mut E::Context<'icc>)),
+        (),
+        &'c mut E::Context<'cc>
+    ) + Clone + Send + Sync + 'static
+{
+    ViewWidget(
+        v,
+        Box::new(m.for_view_cb_if::<RightView::Mutarget,(),MutorFn>(a, f)),
+        PhantomData
+    )
+}
 
 // pub fn view_widget_cb_if_dyn<RightView,LeftArgs,LeftMutor,LeftTarget,MutorFn,RightViewFn,E>(v: RightViewFn, a: LeftArgs, m: LeftMutor, f: MutorFn) -> ViewWidget<
 //     RightView,
